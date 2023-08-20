a1b38a78ac7baa8c69b09d91a38b740418127460
JorgeAbrego <jorge.abrego@gmail.com>
2023-08-20 09:33:16 -0400

    Adding data to mlflow

--- infraestructure/user_data.sh
+++ infraestructure/user_data.sh
@@ -8,7 +8,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin awscli -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
@@ -16,8 +16,8 @@ git clone https://github.com/JorgeAbrego/mlops_zoomcamp_project /home/ubuntu/mlo
 
 cd /home/ubuntu/mlops-project/services
 
-echo "POSTGRES_USER="+$DB_USER | sudo tee .env
-echo "POSTGRES_PASS="+$DB_PASS >> .env
-echo "POSTGRES_HOST="+$DB_HOST >> .env
-echo "POSTGRES_PORT="+$DB_PORT >> .env
-echo "POSTGRES_DB="+$DB_NAME >> .env
+echo "POSTGRES_USER="${DB_USER} | sudo tee .env
+echo "POSTGRES_PASS="${DB_PASS} >> .env
+echo "POSTGRES_HOST="${DB_HOST} >> .env
+echo "POSTGRES_PORT="${DB_PORT} >> .env
+echo "POSTGRES_DB="${DB_NAME} >> .env
